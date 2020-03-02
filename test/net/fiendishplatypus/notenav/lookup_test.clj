cfc5dd7cee4dca9f909668b1f7a4d76fb08ac0c7
aardvark <e.a.latuhin@gmail.com>
2020-03-02 23:51:10 +0300

    Add namespace alias for lookup-test

--- test/net/fiendishplatypus/notenav/lookup_test.clj
+++ test/net/fiendishplatypus/notenav/lookup_test.clj
@@ -1,21 +1,23 @@
 (ns net.fiendishplatypus.notenav.lookup-test
   (:require [clojure.test :refer [deftest is testing]]
-            [net.fiendishplatypus.notenav.lookup :refer [note lookup-position pad-note]]))
+            [net.fiendishplatypus.notenav.lookup :as lookup]))
+
 
 (deftest note-test
   (testing "note should return correct note and octave"
-    (is (= #:app.lookup{:note "E" :octave 1} (note 6 0)))
-    (is (= #:app.lookup{:note "E" :octave 2} (note 6 12)))
-    (is (= #:app.lookup{:note "E" :octave 2} (note 5 7)))
-    (is (= #:app.lookup{:note "E" :octave 2} (note 4 2))))
+    (is (= {::lookup/note "E" ::lookup/octave 1} (lookup/note 6 0)))
+    (is (= {::lookup/note "E" ::lookup/octave 2} (lookup/note 6 12)))
+    (is (= {::lookup/note "E" ::lookup/octave 2} (lookup/note 5 7)))
+    (is (= {::lookup/note "E" ::lookup/octave 2} (lookup/note 4 2))))
 
 
   (testing "lookup-position should return correct position given note and octave"
-    (is (= #:app.lookup{:string 6 :fret 0} (lookup-position "E" 1)))))
+    (is (= {::lookup/string 6 ::lookup/fret 0} (lookup/lookup-position "E" 1)))))
+
 
 (deftest pad-note-test
   (testing "`pad-note` pads note with '-' up to 4 size"
-    (is (= (pad-note "") "|-----"))
-    (is (= (pad-note "E") "|-E---"))
-    (is (= (pad-note "E1") "|-E1--"))
-    (is (= (pad-note "F#1") "|-F#1-"))))
+    (is (= (lookup/pad-note "") "|-----"))
+    (is (= (lookup/pad-note "E") "|-E---"))
+    (is (= (lookup/pad-note "E1") "|-E1--"))
+    (is (= (lookup/pad-note "F#1") "|-F#1-"))))
