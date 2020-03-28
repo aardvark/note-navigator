5f74af4c0c1c0b5d585149aec3e32af51f2c977d
aardvark <e.a.latuhin@gmail.com>
2020-03-28 12:57:32 +0300

    add finger placement circles proto function

--- src/net/fiendishplatypus/notenav/svg.clj
+++ src/net/fiendishplatypus/notenav/svg.clj
@@ -22,37 +22,40 @@
               (string-line 40)
 
               (xml/add-attrs (svg/text "5") :x (+ x 55) :y y)
-              (string-line 60)
+              (string-line (+ 40 20))
 
               (xml/add-attrs (svg/text "4") :x (+ x 75) :y y)
-              (string-line 80)
+              (string-line (+ 40 20 20))
 
               (xml/add-attrs (svg/text "V") :x x :y (+ y 30))
               (fret-line 10)
 
               (xml/add-attrs (svg/text "VI") :x x :y (+ y 60))
-              (fret-line 40)
+              (fret-line (+ 10 30))
 
               (xml/add-attrs (svg/text "VII") :x x :y (+ y 90))
-              (fret-line 70)
+              (fret-line (+ 10 30 30))
 
               (xml/add-attrs (svg/text "VIII") :x x :y (+ y 120))
-              (fret-line 100)
+              (fret-line (+ 10 30 30 30))
 
               (xml/add-attrs (svg/text "IX") :x x :y (+ y 150))
-              (fret-line 130)
+              (fret-line (+ 10 30 30 30 30))
 
-              (svg/circle 100 120 9 :fill "red" :stroke "black" :stroke-width "1")
-              (xml/add-attrs (svg/text "1") :x (+ x 36) :y 125 :stroke "white" :fill "white")
+              (svg/circle (+ x 40 0) (+ y 25 0) 9 :fill "red" :stroke "black" :stroke-width "1")
+              (xml/add-attrs (svg/text "1") :x (+ x 36) :y (+ y 30) :stroke "white" :fill "white")
 
-              (svg/circle 120 120 9 :fill "white" :stroke "black" :stroke-width "1")
-              (xml/add-attrs (svg/text "1") :x (+ x 56) :y 125 :stroke "black" :fill "black")
+              (svg/circle (+ x 40 0) (+ y 25 30 30 30) 9 :fill "white" :stroke "black" :stroke-width "1")
+              (xml/add-attrs (svg/text "4") :x (+ x 36) :y (+ y 30 30 30 30) :stroke "black" :fill "black")
 
-              (svg/circle 100 210 9 :fill "white" :stroke "black" :stroke-width "1")
-              (xml/add-attrs (svg/text "4") :x (+ x 36) :y 215 :stroke "black" :fill "black")
+              (svg/circle (+ x 40 20) (+ y 25 0) 9 :fill "white" :stroke "black" :stroke-width "1")
+              (xml/add-attrs (svg/text "1") :x (+ x 36 20) :y (+ y 30) :stroke "black" :fill "black")
 
-              (svg/circle 120 180 9 :fill "white" :stroke "black" :stroke-width "1")
-              (xml/add-attrs (svg/text "3") :x (+ x 56) :y 185 :stroke "black" :fill "black"))))))
+              (svg/circle (+ x 40 20) (+ y 25 30 30) 9 :fill "white" :stroke "black" :stroke-width "1")
+              (xml/add-attrs (svg/text "3") :x (+ x 36 20) :y (+ y 30 30 30) :stroke "black" :fill "black")
+
+              (svg/circle (+ x 40 20 20) (+ y 25 0) 9 :fill "white" :stroke "black" :stroke-width "1")
+              (xml/add-attrs (svg/text "1") :x (+ x 36 20 20) :y 125 :stroke "black" :fill "black"))))))
 
 
 (defn make-strings
@@ -79,10 +82,9 @@
   [(hline (+ y 10 dy) (+ x 30) (+ x 150))
    (xml/add-attrs (svg/text fret-number) :x x :y (+ y 30 dy))])
 
-(defn make-frets [frets]
+(defn make-frets [x y frets]
   (reduce concat []
-          (let [x 60 y 95
-                a [0 30 60 90]
+          (let [a   [0 30 60 90]
                 dys (into [] (map (fn [a b] [a b]) a frets))]
             (for [dy dys]
               (make-fret x y dy)))))
@@ -91,6 +93,34 @@
   (spit "test-frets.svg"
         (xml/emit
           (apply svg/svg
-                 (make-frets ["I" "II" "III" "IV"])))))
+                 (make-frets 0 0 ["I" "II" "III" "IV"])))))
+
+(comment
+  (spit "test-fret-string.svg"
+        (xml/emit
+          (apply svg/svg
+                 (concat
+                   [{:width 200 :height 200}]
+                   (make-frets 60 95 ["I" "II" "III" "IV"])
+                   (make-strings 60 95))))))
 
 
+(defn finger-circle [x dx y dy & root?]
+  (let [style (if root?
+                {:circle-fill "red"
+                 :text-fill   "white"
+                 :text-stroke "white"}
+                {:circle-fill "white"
+                 :text-fill   "black"
+                 :text-stroke "black"})]
+    [(svg/circle (+ x 40 dx) (+ y 25 dy) 9 :fill (style :circle-fill) :stroke "black" :stroke-width "1")
+     (xml/add-attrs (svg/text "1") :x (+ x 36 dx) :y (+ y 30 dy) :stroke (style :text-stroke) :fill (style :text-fill))]))
+
+(comment
+  (spit "test-circles.svg"
+        (xml/emit
+          (apply svg/svg
+                 (concat []
+                         [{:width 200 :height 200}]
+                         (finger-circle 0 0 0 0 true)
+                         (finger-circle 10 20 10 30))))))
