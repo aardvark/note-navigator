abee0332b10954c92c8b3ff93208333dec58ba69
aardvark <aardvark@bk.ru>
2022-05-21 14:26:32 +0300

    Fix diagram display

--- src/net/fiendishplatypus/notenav/scale.clj
+++ src/net/fiendishplatypus/notenav/scale.clj
@@ -15,17 +15,36 @@
    produce list of notes in a scale"
   [root-note scale]
   (loop [scale scale
-         note root-note
-         acc '()]
+         note  root-note
+         acc   '()]
     (if (empty? scale)
       (reverse acc)
       (recur
-       (rest scale)
-       (l/upscale note (first scale))
-       (conj acc note)))))
+        (rest scale)
+        (l/upscale note (first scale))
+        (conj acc note)))))
 
 (comment
   (scale {::l/note "C" ::l/octave 3} major))
+
+(defn scale->def
+  [root-note scale-type]
+  (zipmap (scale root-note scale-type) (range 1 9)))
+
+(defn note-in-scale
+  "Given a scale-def and a note return a note number in scale"
+  [scale-def note]
+  (get scale-def note))
+
+(comment
+  (get (scale->def {::l/note "C" ::l/octave 3} major) {::l/note "E" ::l/octave 3})
+
+  (let [lookup (scale->def {::l/note "C" ::l/octave 3} major)
+        lookup (partial note-in-scale lookup)]
+    (map #(apply (comp lookup l/note) %)
+         [[3 5] [3 7] [2 5] [2 8] [2 5] [3 7] [3 5]]
+         #_[[3 5] [3 7] [2 5] [2 8] [1 5] [1 8] [1 5] [2 8] [2 5] [3 7]]))
+  )
 
 (defn number-to-strings
   ([input]
@@ -50,7 +69,7 @@
   (number-to-strings [5 2] [3 4])
   ;; => ("" " " "" "" " " "")
   (number-to-strings [2 4]))
-  ;; => ("" "" " " "" " " "")
+;; => ("" "" " " "" " " "")
 
 
 (defn to-int
@@ -59,15 +78,15 @@
 
 
 (def empty-fret
-  {:1 '("" "" "" "" "" "")
-   :2 '("" "" "" "" "" "")
-   :3 '("" "" "" "" "" "")
-   :4 '("" "" "" "" "" "")
-   :5 '("" "" "" "" "" "")
-   :6 '("" "" "" "" "" "")
-   :7 '("" "" "" "" "" "")
-   :8 '("" "" "" "" "" "")
-   :9 '("" "" "" "" "" "")
+  {:1  '("" "" "" "" "" "")
+   :2  '("" "" "" "" "" "")
+   :3  '("" "" "" "" "" "")
+   :4  '("" "" "" "" "" "")
+   :5  '("" "" "" "" "" "")
+   :6  '("" "" "" "" "" "")
+   :7  '("" "" "" "" "" "")
+   :8  '("" "" "" "" "" "")
+   :9  '("" "" "" "" "" "")
    :10 '("" "" "" "" "" "")
    :11 '("" "" "" "" "" "")
    :12 '("" "" "" "" "" "")})
@@ -75,35 +94,55 @@
 
 (defn notes-to-positions
   [notes]
-  (let [coll (mapcat (fn [[n {::l/keys [note octave]}]]
-                       (map #(assoc % :octave-position (str n))
-                            (l/lookup-position note octave)))
-                notes)
+  (let [coll  (mapcat (fn [[n {::l/keys [note octave]}]]
+                        (map #(assoc % :octave-position (str n))
+                             (l/lookup-position note octave)))
+                      notes)
         frets (persistent!
-               (reduce
-                (fn [acc x]
-                  (let [fret (::l/fret x)
-                        [strs octns] (get acc fret [[] []])
-                        strs (conj strs (::l/string x))
-                        octns (conj octns (:octave-position x))]
-                    (assoc! acc fret [strs octns])))
-                (transient {}) coll))]
+                (reduce
+                  (fn [acc x]
+                    (let [fret  (::l/fret x)
+                          [strs octns] (get acc fret [[] []])
+                          strs  (conj strs (::l/string x))
+                          octns (conj octns (:octave-position x))]
+                      (assoc! acc fret [strs octns])))
+                  (transient {}) coll))]
 
     (into (sorted-map-by #(< (to-int %1) (to-int %2)))
           (map (fn [[k [v vv]]]
                  [(keyword (str k)) (number-to-strings v vv)])
                frets))))
 
+(defn- overwrite-if-blank
+  "Merge values function that will overwrite value if it is blank"
+  [val-in-res val-in-later]
+  (map (fn [a b] (if (s/blank? b) a b))
+       val-in-res val-in-later))
 
 (defn to-diagram
   [tonic scale-type]
-  (apply merge-with
-         (fn [val-in-res val-in-later]
-           (map (fn [a b] (if (s/blank? b) a b))
-                val-in-res val-in-later))
-         (cons
-          empty-fret
-          (for [o (list 0 1 2 3 4 5)]
-            (notes-to-positions (zipmap (list "R" "2" "3" "4" "5" "6" "7" "R")
-                                        (scale {::l/note tonic ::l/octave o} scale-type)))))))
+  (sort-by (fn [item] (Integer/parseInt (name (key item))))
+           (apply merge-with
+                  overwrite-if-blank
+                  (cons
+                    empty-fret
+                    (for [o (list 0 1 2 3 4 5)]
+                      (notes-to-positions (zipmap (list "R" "2" "3" "4" "5" "6" "7" "R")
+                                                  (scale {::l/note tonic ::l/octave o} scale-type))))))))
 
+(comment
+  (to-diagram "A" major)
+  (notes-to-positions (zipmap (list "R" "2" "3" "4" "5" "6" "7" "R")
+                              (scale {::l/note "A" ::l/octave 1} major)))
+
+  (sort-by (fn [item] (Integer/parseInt (name (key item))))
+           (merge-with
+             overwrite-if-blank
+             {:1 '("" "" "" "" "" "")}
+             {:1 '("5" "R" "" "" "" "")}
+             {:1 '("" "" "4" "" "" "")}
+             {:0 '("" "" "" "" "" "")}
+             {:0 '("5" "R" "" "" "" "")}
+             {:0 '("" "" "4" "" "" "")}
+             ))
+  )
